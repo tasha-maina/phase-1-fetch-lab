3027816c079f3176b06b8e1b7e0a79369cbc2d5e
tasha maina <tashabanks271@gmail.com>
2025-07-07 20:46:53 +0300

    Completed fetch lab

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((booksData) => renderBooks(booksData));
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
